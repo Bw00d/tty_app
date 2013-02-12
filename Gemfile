802e905e70f50209069428f049400e2e45dcd3b2
Brentwood Reid <bwoodreid@gmail.com>
2013-02-12 14:56:30 +0700

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -12,8 +12,7 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
   gem 'guard-rspec', '1.2.1'
-  gem 'guard-spork', '1.2.0'  
-  gem 'spork', '0.9.2'
+  # I moved guard and spork to group test.
 end
 
 # Gems used only for assets and not required
@@ -32,6 +31,8 @@ group :test do
   gem 'launchy', '2.1.0'
   gem 'rb-fsevent', '0.9.1', :require => false
   gem 'growl', '1.0.3'
+  gem 'guard-spork', '1.2.0'  
+  gem 'spork', '0.9.2' 
 
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
-class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,      only: :destroy
   
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show 
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(params[:user])    
       sign_in @user
       flash[:success] = "Profile updated!"
       redirect_to @user
@@ -47,12 +48,6 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user 
-      unless signed_in?
-      store_location
-      redirect_to signin_path, notice: "Please sign in." 
-    end
-
     def correct_user
         @user = User.find(params[:id])
         redirect_to(root_path) unless current_user?(@user)
@@ -60,8 +55,8 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to root_path unless current_user.admin?
+    
     end
   end
-end
 
 
