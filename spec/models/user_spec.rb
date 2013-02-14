41492bc6d37f3b7cd1ebbf2583a49929c9967035
Brentwood Reid <bwoodreid@gmail.com>
2013-02-14 15:23:16 +0700

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,13 @@ describe User do
 	  it { should respond_to(:authenticate) }
     it { should respond_to(:microposts) }
     it { should respond_to(:feed) }
+    it { should respond_to(:relationships) }
+    it { should respond_to(:followed_users) }
+    it { should respond_to(:reverse_relationships) }
+    it { should respond_to(:followers) }
+    it { should respond_to(:following?) }
+    it { should respond_to(:follow!) }
+    it { should respond_to(:unfollow!) }
 
 	  it { should be_valid }
     it { should_not be_admin }
@@ -27,7 +34,7 @@ describe User do
       it "should not allow access to admin" do
         expect do 
           User.new(admin: "1")
-        end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)         
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)         
       end      
     end
 
@@ -135,18 +142,55 @@ describe User do
       microposts = @user.microposts
       @user.destroy
       microposts.each do |micropost|
+        
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
 
     describe "status" do
-      let(:unfollowed_micropost) do
+      let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
 
-      its(:feed) { should include(older_micropost) }
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
       its(:feed) { should include(newer_micropost) }
-      its(:feed) { should_not include(unfollowed_micropost) }      
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+  
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
